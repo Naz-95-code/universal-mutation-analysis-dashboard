c5b161412194bfb7873f6f08e03a315c140edefb
Chinazo Christella Orji <chinazokristel@gmail.com>
2026-03-03 10:08:58 -0500

    Refactored extract_spectrum.sh to support structured directory input and fixed path handling

--- scripts/extract_spectrum.sh
+++ scripts/extract_spectrum.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
+set -e
+
 module load bcftools/1.21
 
-for file in *.vcf
+# Check argument
+if [ -z "$1" ]; then
+    echo "Usage: bash scripts/extract_spectrum.sh <vcf_directory>"
+    exit 1
+fi
+
+input_dir=$1
+output_dir="results/spectra"
+
+mkdir -p "$output_dir"
+
+for file in "$input_dir"/*.vcf
 do
     echo "Processing $file"
+
+    filename=$(basename "$file" .vcf)
 
     bcftools view -v snps "$file" | \
     bcftools query -f '%REF>%ALT\n' | \
     sort | uniq -c | \
-    awk '{
-            total += $1
-            counts[$2] = $1
-         }
-         END {
-            for (type in counts)
-                printf "%s\t%d\t%.6f\n", type, counts[type], counts[type]/total
-         }' \
-    > "${file%.vcf}_spectrum.txt"
+    awk '
+    {
+        total += $1
+        counts[$2] = $1
+    }
+    END {
+        for (type in counts)
+            printf "%s\t%d\t%.6f\n", type, counts[type], counts[type]/total
+    }' \
+    > "$output_dir/${filename}_spectrum.txt"
 
 done
 
